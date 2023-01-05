7ce385d0c7792299194355e3c950a3ad5c45a1b9
M0skus <85843306+M0skus@users.noreply.github.com>
2023-01-05 12:52:50 +0100

    Add files via upload

---
+++ link.js
@@ -0,0 +1,8 @@
+function produkter() {
+    window.location.href = "https://www.youtube.com/watch?v=6_GvlikmZzc";
+}
+
+
+function main() {
+    window.location.href = "https://i0.wp.com/bloody-disgusting.com/wp-content/uploads/2018/08/nun-youtube.png?w=1065&ssl=1";
+}
